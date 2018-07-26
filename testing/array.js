4c0a4b80c4546109deccd4e4f4d4ce968b785058
Anna Ste <stepashko.ann@gmail.com>
2018-07-26 13:00:09 +0300

    add Set

--- testing/array.js
+++ testing/array.js
@@ -8,3 +8,25 @@
 
 const filterValue = (obj, key, value)=> obj.filter(v => v[key] === value);
 
+//sort array using another array
+        let options = rUtils.reportTypeService.typesByPeriod(this.report.periodId);
+        let exclude = new Set([4,5,8,9,10,11,12,13,19,21,24,28,29,32,33,34,35,36,37,39,41,42]);
+
+        const arr = options.filter( item => {
+          if( exclude.has(item.id) ){ return item };
+        });
+
+        enabledForAutoSum: function() {
+          let arr = this.report_type.table;
+          let exclude = new Set([1,4,22,41]);
+
+          if (exclude.has(this.report_type.id)) {
+            return false;
+          }
+
+          for(const item of arr) {
+            if(item.style != null && item.style.includes("sum")) {
+              return true;
+            }
+          }
+        }
