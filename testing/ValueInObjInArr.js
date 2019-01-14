b8af656f3bbe2b2fb6c847193bf6dfce9c3d27ad
Anna Ste <stepashko.ann@gmail.com>
2019-01-14 13:27:52 +0200

    value in obj in arr

---
+++ testing/ValueInObjInArr.js
@@ -0,0 +1,5 @@
+// если нужно найти value in obj in arr
+            if (arr.some(e => e.value === 'aaa')) {
+                console.log('AAAAA')
+            }
+ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
