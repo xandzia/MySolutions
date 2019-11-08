75c983055e6b4c12a3e23472511ffd9d11ee1402
Anna Ste <stepashko.ann@gmail.com>
2019-11-08 16:33:30 +0200

    add method get unique

--- testing/onlyUnique.js
+++ testing/onlyUnique.js
@@ -1,3 +1,11 @@
         const options = [........]
         const onlyUnique = (value, index, self) => { return self.indexOf(value) === index };
         return options.filter(onlyUnique);
+
+var fruits = [“banana”, “apple”, “orange”, “watermelon”, “apple”, “orange”, “grape”, “apple”];
+// First method
+var uniqueFruits = Array.from(new Set(fruits));
+console.log(uniqueFruits); // returns [“banana”, “apple”, “orange”, “watermelon”, “grape”]
+// Second method
+var uniqueFruits2 = […new Set(fruits)];
+console.log(uniqueFruits2); // returns [“banana”, “apple”, “orange”, “watermelon”, “grape”]
